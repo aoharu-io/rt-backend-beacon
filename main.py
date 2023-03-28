9dee50138fd8317fed31211c1cdcb3a260f7a559
Takagi Tasuku <tasuren@outlook.jp>
2023-03-28 19:18:16 +0900

    improve: argparseで引数の処理をするように実装。

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 from typing import Any
 
-from sys import argv
+from argparse import ArgumentParser
 
 from ipcs import Server, logger
 
@@ -15,8 +15,21 @@ from common.lib.log import set_output_handler
 from common.lib.backend import IPCS_SERVER_ID, is_bot
 
 
+parser = ArgumentParser(
+    prog="RT Discord Botのためのビーコンの実装",
+    description="バックエンドのAPI実装およびBotクライアント実装の通信に必要なサーバーがこれです。"
+)
+
+parser.add_argument("-H", "--host", default="127.0.0.1", help="提供対象のホスト名です。")
+parser.add_argument("-p", "--port", type=int, default=8765, help="使うポートです。")
+
+args = parser.parse_args()
+
+
 set_stream_handler(logger)
 set_output_handler(logger, default="log/ipcs.log")
+logger.info("RT Discord Bot （ビーコンの実装） が起動中...")
+
 
 with open("secret.key", "rb") as f:
     chiper = ChiperManager(f.read())
@@ -31,15 +44,16 @@ def shared_data(_, attr: str, *args: Any, **kwargs: Any) -> Any:
     return getattr(data, attr)(*args, **kwargs)
 
 
-async def _process_request(_, headers: Headers) -> ...:
+async def _process_request(headers: Headers) -> ...:
     if "Authorization" not in headers or \
             not is_bot(chiper, headers["Authorization"]):
-        logger.info("Kicked invalid request.")
+        logger.info("無効なリクエストを拒否しました。")
+        logger.debug("拒否されたリクエストのヘッダ：%s", headers)
         return (401, (), bytes())
 
 
-logger.info(f"Host: {argv[1]}, Port: {argv[2]}")
+logger.info("提供を開始しました。")
 server.run(
-    argv[1], int(argv[2]),
+    args.host, args.port,
     process_request=_process_request
 )
